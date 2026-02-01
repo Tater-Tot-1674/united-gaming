d621730e93ed12387eaa45f05961e6c4b845472b
Tater-Tot-1674 <tatertotgaming1234@gmail.com>
2026-01-31 20:51:10 -0700

    Update bracket.js

--- js/bracket.js
+++ js/bracket.js
@@ -1,1 +1,29 @@
+const bracketContainer = document.getElementById('bracket');
+
+async function fetchBracket() {
+  try {
+    const res = await fetch('/data/bracket.json', { cache: 'no-store' });
+    const data = await res.json();
+    renderBracket(data);
+  } catch (err) {
+    console.error('Bracket load error:', err);
+  }
+}
+
+function renderBracket(data) {
+  bracketContainer.innerHTML = '';
+
+  data.forEach(match => {
+    const div = document.createElement('div');
+    div.className = 'bracket-match';
+    div.innerHTML = `
+      <strong>${match.player1}</strong> vs <strong>${match.player2}</strong>
+      <br>Winner: ${match.winner || 'TBD'}
+    `;
+    bracketContainer.appendChild(div);
+  });
+}
+
+fetchBracket();
+setInterval(fetchBracket, 5000);
 
