a908c6f68e44c814d1257f1732a2b3392ba157ad
Tater-Tot-1674 <tatertotgaming1234@gmail.com>
2026-01-31 20:51:28 -0700

    Update announcements.js

--- js/announcements.js
+++ js/announcements.js
@@ -1,1 +1,29 @@
+const container = document.getElementById('announcements');
+
+async function fetchAnnouncements() {
+  try {
+    const res = await fetch('/data/announcements.json', { cache: 'no-store' });
+    const data = await res.json();
+    renderAnnouncements(data.reverse());
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function renderAnnouncements(data) {
+  container.innerHTML = '';
+
+  data.forEach(a => {
+    const div = document.createElement('div');
+    div.className = 'announcement';
+    div.innerHTML = `
+      <strong>${a.author}</strong> • ${new Date(a.date).toLocaleString()}<br>
+      ${a.content}
+    `;
+    container.appendChild(div);
+  });
+}
+
+fetchAnnouncements();
+setInterval(fetchAnnouncements, 5000);
 
