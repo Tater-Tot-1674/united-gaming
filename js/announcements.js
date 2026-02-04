a0dfd542465b2dc4b59d5d4bcf6a47afb307719e
Tater-Tot-1674 <tatertotgaming1234@gmail.com>
2026-02-04 09:26:01 -0700

    Update announcements.js

--- js/announcements.js
+++ js/announcements.js
@@ -12,17 +12,23 @@ async function fetchAnnouncements() {
 
 function updateAnnouncements(data) {
   announcementsContainer.innerHTML = '';
+
+  if (!Array.isArray(data)) return;
+
   data.forEach(post => {
     const div = document.createElement('div');
     div.className = 'announcement';
+
     div.innerHTML = `
-      <h3>${post.author}</h3>
-      <p>${post.content}</p>
-      <small>${new Date(post.date).toLocaleString()}</small>
+      <h3>${post.author || 'Unknown'}</h3>
+      <p>${post.content || ''}</p>
+      <small>${post.date ? new Date(post.date).toLocaleString() : ''}</small>
     `;
+
     announcementsContainer.appendChild(div);
   });
 }
 
 fetchAnnouncements();
 setInterval(fetchAnnouncements, 5000);
+
